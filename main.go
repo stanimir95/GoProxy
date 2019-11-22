c05fb5f149918e69da83732bff94f092a864235f
stanimir petrov <stanimir_petrov95@abv.bg>
2019-11-22 23:07:21 +0200

    fixed server discovery

--- main.go
+++ main.go
@@ -1,121 +1,57 @@
 package main
 
 import (
-	"crypto/tls"
 	"flag"
 	"fmt"
-	"log"
-	"net"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 	"strings"
 )
 
-//Balancers - taken from -b input
-type Balancers struct {
-	serverNames     string
-	chosenBalancers []string
+//FixedHTTPPrefix please stop
+var FixedHTTPPrefix []string
+
+func getServers() []string {
+
+	var serverList string
+	// var fixedHTTPPrefix []string
+
+	flag.StringVar(&serverList, "b", "", "Load balancer goes here")
+	flag.Parse()
+
+	servers := strings.Split(serverList, " ")
+	for _, server := range servers {
+		server = "http://" + server
+		FixedHTTPPrefix = append(FixedHTTPPrefix, server)
+	}
+	return FixedHTTPPrefix
 }
 
-//Alive - available load balancers after health check
-type Alive struct {
-	areAlive []string
-}
+func checkServerStatus(fixedHTTPPrefix []string) []string {
 
-func randomHost() {
-	var a Alive
-	for i, j := range a.areAlive {
-		fmt.Println("randomHost")
-		fmt.Println(i)
-		_ = j
-	}
-}
-
-func reverseProxy() {
-
-	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
-		Scheme: "https",
-		Host:   "zamunda.net",
-	})
-	proxy.Transport = &http.Transport{DialTLS: dialTLS}
-
-	// Change req.Host so badssl.com host check is passed
-	director := proxy.Director
-	proxy.Director = func(req *http.Request) {
-		director(req)
-		req.Host = req.URL.Host
-	}
-
-	log.Fatal(http.ListenAndServe("127.0.0.1:3000", proxy))
-}
-
-func dialTLS(network, addr string) (net.Conn, error) {
-	conn, err := net.Dial(network, addr)
-	if err != nil {
-		return nil, err
-	}
-
-	host, _, err := net.SplitHostPort(addr)
-	if err != nil {
-		return nil, err
-	}
-	cfg := &tls.Config{ServerName: host}
-
-	tlsConn := tls.Client(conn, cfg)
-	if err := tlsConn.Handshake(); err != nil {
-		conn.Close()
-		return nil, err
-	}
-
-	cs := tlsConn.ConnectionState()
-	cert := cs.PeerCertificates[0]
-
-	// Verify here
-	cert.VerifyHostname(host)
-	log.Println(cert.Subject)
-
-	return tlsConn, nil
-}
-
-func healthCheck(b Balancers) {
-	var a Alive
-	doExist := map[int]bool{}
-	result := []int{}
-	for {
-		for _, s := range b.chosenBalancers {
-			_, err := http.Get(s)
-			if err == nil {
-				a.areAlive = append(a.areAlive, s)
-			}
+	var liveServers = fixedHTTPPrefix
+	for i, server := range liveServers {
+		req, err := http.Get(server)
+		if err == nil {
+			liveServers = append(liveServers, server)
+		} else {
+			liveServers[i] = liveServers[len(liveServers)-1]
+			return liveServers[:len(liveServers)-1]
 		}
-		fmt.Println("")
-		fmt.Println("Are Alive:")
-		for i, j := range a.areAlive {
-			fmt.Println(i)
-			fmt.Println(j)
-		}
+
+		defer req.Body.Close()
 	}
+	return liveServers
+
 }
 
 func main() {
-	// var serverNames string
-	var b Balancers
-	flag.StringVar(&b.serverNames, "b", "", "Input balancers")
-	flag.Parse()
+	fmt.Println("	fmt.Println(getServers())")
+	fmt.Println(getServers())
 
-	serverNameWithPrefix := strings.Split(b.serverNames, " ")
-	for _, i := range serverNameWithPrefix {
-		i = "http://" + i
-		b.chosenBalancers = append(b.chosenBalancers, i)
-	}
+	fmt.Println("	fmt.Println(checkServerStatus(FixedHTTPPrefix))	")
+	fmt.Println(checkServerStatus(FixedHTTPPrefix))
 
-	fmt.Println("Chosen Balancers")
-	for _, server := range b.chosenBalancers {
-		fmt.Println(server)
-	}
+	fmt.Println("	fmt.Println(FixedHTTPPrefix)")
+	fmt.Println(FixedHTTPPrefix)
 
-	go healthCheck(b)
-
-	reverseProxy()
 }
